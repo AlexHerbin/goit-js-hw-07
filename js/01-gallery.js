e817d9a99a096faeedc12d30057107e19da0231d
AlexHerbin <101976159+AlexHerbin@users.noreply.github.com>
2022-09-12 18:32:50 +0300

    ДЗ по ТЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-const imgEl = document.querySelector('.gallery')
-console.log(imgEl);
-const GalleryGrid = createGalleryGrid(galleryItems)
-imgEl.innerHTML = GalleryGrid;
-
+const containerImg = document.querySelector('.gallery');
+const galleryGrid = createGalleryGrid(galleryItems);
+containerImg.innerHTML = galleryGrid;
 
 function createGalleryGrid(items) {
-    return items.map(item => `<ul class="gallery__item"> <li class="gallery__link"> <img class="gallery__image" src = "${item.preview}" alt = "${item.description}"></img> </li> </ul>`).join("");
-}
+    return items.map(item => `<div class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src = "${item.preview}" data-source = "${item.original}" alt = "${item.description}"/> </a> </div>`).join("");
+};
 
-console.log(GalleryGrid);
+containerImg.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName !== "IMG") {
+        return
+    };
+
+    const originalImg = evt.target.dataset.source;
+
+    const modal = basicLightbox.create(`
+    <img src="${originalImg}">
+    `,
+        {
+            onShow: () => { window.addEventListener('keydown', onEscPress)},
+            
+            onClose: () => { window.removeEventListener('keydown', onEscPress)}
+        }
+    );
+    modal.show();
+
+    function onEscPress(evt) {
+        const ESC_KEY_COD = 'Escape';
+        if (evt.code === ESC_KEY_COD) {
+            modal.close()
+        }
+    }
+};
+
+
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerImg = document.querySelector('.gallery');
+const galleryGrid = createGalleryGrid(galleryItems);
+containerImg.innerHTML = galleryGrid;
+
+function createGalleryGrid(items) {
+    return items.map(item => `<div class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src = "${item.preview}"  alt = "${item.description}"/> </a> </div>`).join("");
+};
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
